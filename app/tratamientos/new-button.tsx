59a4bc2e532fd8b37bdfc7c12f1655f9120ebe8f
Eduardo Parra <eduparra90@gmail.com>
2023-06-16 00:13:18 +0200

    add select product

--- app/tratamientos/new-button.tsx
+++ app/tratamientos/new-button.tsx
@@ -4,22 +4,27 @@ import { Button } from '@tremor/react';
 import { PlusIcon } from '@heroicons/react/24/solid';
 import Modal from './modal';
 import { useCallback, useState } from 'react';
+import { Product } from 'models';
 
-const NewButton = () => {
+const NewButton = ({ products }: { products: Product[] }) => {
   const [open, setOpen] = useState(false);
 
-  const handleClose = useCallback(
-    () => {
-      setOpen(false);
-    },
-    [setOpen]
-  );
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
 
   const handleClick = () => setOpen(true);
   return (
     <>
-      <Button className='ml-auto' icon={PlusIcon} variant='secondary' onClick={handleClick}>Añadir</Button>
-      <Modal open={open} onClose={handleClose} />
+      <Button
+        className="ml-auto"
+        icon={PlusIcon}
+        variant="secondary"
+        onClick={handleClick}
+      >
+        Añadir
+      </Button>
+      <Modal open={open} onClose={handleClose} products={products} />
     </>
   );
 };
--- app/tratamientos/page.tsx
+++ app/tratamientos/page.tsx
@@ -4,34 +4,49 @@ import RegistryTable from './table';
 import Search from '../search';
 import NewButton from './new-button';
 
-
 const dataFormatter = (number: number) =>
   Intl.NumberFormat('es').format(number).toString();
 
-
-const RegistryPage = async ({ searchParams }: {
+const RegistryPage = async ({
+  searchParams
+}: {
   searchParams: { q: string };
 }) => {
   const search = searchParams.q ?? '';
   const registry = await queryBuilder
     .selectFrom('tratamientos')
     .innerJoin('productos', 'tratamientos.productoid', 'productos.id')
-    .select(['tratamientos.id', 'tratamientos.cultivo', 'productos.nombre', 'tratamientos.para', 'tratamientos.fecha', 'tratamientos.dosis', 'tratamientos.siguiente', 'tratamientos.aclaraciones'])
+    .select([
+      'tratamientos.id',
+      'tratamientos.cultivo',
+      'productos.nombre',
+      'tratamientos.para',
+      'tratamientos.fecha',
+      'tratamientos.dosis',
+      'tratamientos.siguiente',
+      'tratamientos.aclaraciones'
+    ])
     .where('tratamientos.cultivo', 'like', `%${search}%`)
     .orderBy('tratamientos.fecha', 'desc')
     .execute();
 
+  const products = await queryBuilder
+    .selectFrom('productos')
+    .select(['id', 'nombre'])
+    .orderBy('nombre', 'asc')
+    .execute();
+
   return (
-    <main className='p-4 md:p-10 mx-auto max-w-7xl'>
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Flex>
         <div>
           <Title>Tratamientos</Title>
           <Text>Registro de tratamientos</Text>
         </div>
-        <NewButton />
+        <NewButton products={products} />
       </Flex>
       <Search />
-      <Card className='mt-6'>
+      <Card className="mt-6">
         {/* @ts-expect-error Server Component */}
         <RegistryTable registry={registry} />
       </Card>
